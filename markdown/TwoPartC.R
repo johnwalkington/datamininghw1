30d98e529ed38d9c1477023625235411cb628061
parkergauthier <97997086+parkergauthier@users.noreply.github.com>
2022-02-09 18:51:07 -0600

    two and four doneski

---
+++ markdown/TwoPartC.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(mosaic)
+
+billboard = read.csv("billboard.csv")
+
+billboard_simp = billboard %>%
+  select(song, performer, song_id, weeks_on_chart, year)
+
+billboard_10w = billboard_simp %>%
+  filter(weeks_on_chart == 10) %>%
+  group_by(performer) %>%
+  summarize(count = n()) %>%
+  filter(count >= 30) %>%
+  arrange(desc(count))
+
+ggplot(billboard_10w) +
+  geom_col(aes(x = performer, y = count, color = performer)) +
+  coord_flip() +
+  labs(title = "Number of Songs on Billboard's Top 100 For At Least 10 Weeks by Top Artists*", x = "Performer", y = "Number of Songs", caption = "*Data was filtered for artists who had at least 30 songs that appeared for at least 10 weeks.") +
+  theme(legend.position = "none")
